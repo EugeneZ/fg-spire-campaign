f96755891e1fd02b7c8969bfe7b50c12679b7158
Eugene Zaretskiy <eugene.zar@gmail.com>
2022-09-15 19:28:49 -0400

    Initial commit

---
+++ CampaignRegistry.lua
@@ -0,0 +1,24 @@
+{
+	["OptREVL"] = "on",
+	["OptCHATICONTHEME"] = "square",
+	["windowstate"] = {
+		["tokenbag"] = {
+			["lastfilter"] = "all",
+		},
+	},
+	["OptCHATFONTCOLORS"] = "dark",
+	["sidebarversion"] = 2,
+	["sidebarvisibility"] = 0,
+	["sidebarexpand"] = {
+		[1] = "tool",
+		[2] = "campaign",
+		[4] = "player",
+		[3] = "library",
+	},
+	["OptMANUALROLL"] = "off",
+	["OptDDCL-custom"] = "",
+	["OptSHPW"] = "on",
+	["setup"] = true,
+	["OptPSIN"] = "on",
+	["OptHRDD"] = "",
+}
