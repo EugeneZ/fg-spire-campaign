5b9a3265a09e38103b17e1f614bf566205b57e86
Eugene Zaretskiy <eugene.zar@gmail.com>
2022-12-11 23:00:01 -0500

    synced commit

--- CampaignRegistry.lua
+++ CampaignRegistry.lua
@@ -1,24 +1,38 @@
 {
-	["OptREVL"] = "on",
-	["OptCHATICONTHEME"] = "square",
 	["windowstate"] = {
 		["tokenbag"] = {
 			["lastfilter"] = "all",
 		},
 	},
-	["OptCHATFONTCOLORS"] = "dark",
 	["sidebarversion"] = 2,
+	["OptDDCL-custom"] = "",
+	["OptSHPW"] = "on",
+	["OptPSIN"] = "on",
+	["OptHRDD"] = "",
+	["OptCHATGMICONCOLOR"] = "red",
+	["OptCHATGMICON"] = "default",
+	["OptCHATFONTCOLORS"] = "off",
 	["sidebarvisibility"] = 1,
+	["setup"] = true,
+	["OptREVL"] = "on",
+	["OptCHATICONTHEME"] = "square",
+	["OptMANUALROLL"] = "off",
+	["desktopdicedisable"] = {
+		["d100"] = true,
+		["dF"] = true,
+	},
+	["colortables"] = {
+		[""] = {
+			["color"] = "000000",
+			["diceskin"] = 0,
+			["dicebodycolor"] = "272100",
+			["dicetextcolor"] = "C2FF5A",
+		},
+	},
 	["sidebarexpand"] = {
 		[1] = "campaign",
 		[2] = "library",
 		[3] = "player",
 		[4] = "tool",
 	},
-	["OptMANUALROLL"] = "off",
-	["OptDDCL-custom"] = "",
-	["OptSHPW"] = "on",
-	["setup"] = true,
-	["OptPSIN"] = "on",
-	["OptHRDD"] = "",
 }
